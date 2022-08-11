c448ff01029f3ae57b9e49d97b2a5ecfd435ab4e
codu2 <khub0929@gmail.com>
2022-08-11 17:24:40 +0900

    recoil을 이용해 localStorage에 강의시간표 data 연동

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,22 @@
 import { atom } from "recoil";
 
+const localStorageEffect =
+  (key) =>
+  ({ setSelf, onSet }) => {
+    // setSelf - store 값을 스스로 set하는 기능, onSet - localStorage가 변경되었을 때 실행
+    const savedValue = localStorage.getItem(key);
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    // newValue - 새로운 값, _ - 기존 값, isReset - reset 여부를 알려주는 boolean 값
+    onSet((newValue, _, isReset) => {
+      isReset
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
 export const timeTableState = atom({
   key: "timeTableState",
   default: {
@@ -9,4 +26,5 @@ export const timeTableState = atom({
     thu: [],
     fri: [],
   },
+  effects: [localStorageEffect("timeTable")],
 });
