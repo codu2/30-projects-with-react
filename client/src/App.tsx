e612abdc48dd9c7b0177355fef6dc12a80400cb0
codu2 <khub0929@gmail.com>
2022-09-24 12:10:42 +0900

    axios baseURL 설정 및 note의 이미지 사이즈를 위한 Global css 추가, 재사용이 가능한 Box, Button, Flex 컴포넌트 생성 및 react-quill을 이용한 웹에디터인 Editor 컴포넌트 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,25 @@
-import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Routes from "./routes";
+import { css, Global } from "@emotion/react";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8080";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <>
+      <Global
+        styles={css`
+          .note-content img {
+            max-width: 100%;
+          }
+        `}
+      />
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </>
   );
 }
 
