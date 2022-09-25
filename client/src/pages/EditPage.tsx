219c85aca9556cfb6fbd330aa44546a79dd2ba6b
codu2 <khub0929@gmail.com>
2022-09-25 17:01:52 +0900

    특정 note를 수정할 수 있는 EditPage 및 note list를 관리할 수 있는 ManagerPage 생성

--- client/src/pages/EditPage.tsx
+++ client/src/pages/EditPage.tsx
@@ -1,9 +1,59 @@
-import { useParams } from "react-router-dom";
+import { useState, useCallback, useEffect } from "react";
+import Box from "../components/Box";
+import Editor from "../components/Editor";
+import Button from "../components/Button";
+import Flex from "../components/Flex";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 const EditPage = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
+  const [edit, setEdit] = useState<string | null>(null);
 
-  return <>edit</>;
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(`/${id}`);
+        setEdit(data.content);
+      } catch (error) {
+        alert((error as any).response.data.msg);
+        navigate("/");
+      }
+    })();
+  }, []);
+
+  const handleSubmit = useCallback(async () => {
+    try {
+      await axios.put(`/${id}`, {
+        content: edit,
+      });
+      alert("수정되었습니다.");
+      navigate(`/${id}`);
+    } catch (error) {
+      alert((error as any).response.data.msg);
+      navigate("/");
+    }
+  }, [edit, id]);
+
+  if (edit === null) {
+    return <></>;
+  }
+
+  return (
+    <Box p="16px">
+      <h3>ID : {id}</h3>
+      <Editor value={edit} onChange={setEdit} />
+      <Flex style={{ gap: "8px" }} justifyContent="flex-end">
+        <Button mt="8px" onClick={handleSubmit}>
+          UPDATE
+        </Button>
+        <Button mt="8px" onClick={() => navigate(`/${id}`)}>
+          CANCLE
+        </Button>
+      </Flex>
+    </Box>
+  );
 };
 
 export default EditPage;
